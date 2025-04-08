14f9c537454d9e723ea8e51b52d8d38087109995
Leonardo Reginato <63908640+leonardo-reginato@users.noreply.github.com>
2025-04-08 16:21:45 -0500

    fix: remove useless import

--- cryptovision/__init__.py
+++ cryptovision/__init__.py
@@ -1,4 +1,3 @@
-from cryptovision import config  # noqa: F401
 from cryptovision import tools  # noqa: F401
 from cryptovision import models  # noqa: F401
 from cryptovision import dataset  # noqa: F401
