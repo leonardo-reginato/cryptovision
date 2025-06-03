b293023f38ab9356dfa5f05a151d7528d8f8e040
Leonardo Reginato <63908640+leonardo-reginato@users.noreply.github.com>
2025-06-03 12:23:41 -0500

    update repo for better use

--- cryptovision/__init__.py
+++ cryptovision/__init__.py
@@ -1,3 +1,3 @@
-from cryptovision import tools  # noqa: F401
+from cryptovision import utils  # noqa: F401
 from cryptovision import models  # noqa: F401
 from cryptovision import dataset  # noqa: F401
